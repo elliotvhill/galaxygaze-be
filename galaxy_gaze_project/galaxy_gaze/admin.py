19a1b691a7d66da9859bd46e99cabd1b3d17162c
Elliot <ellthrowaway2@icloud.com>
2023-07-26 10:38:13 -0400

    set up admin console

--- galaxy_gaze_project/galaxy_gaze/admin.py
+++ galaxy_gaze_project/galaxy_gaze/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import CelestialBody, CosmicEvent
 
-# Register your models here.
+admin.site.register(CelestialBody)
+admin.site.register(CosmicEvent)
