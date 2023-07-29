b8fc1f3a6bbb03318398d872c56f9826d04bfc97
Elliot <ellthrowaway2@icloud.com>
2023-07-29 12:03:20 -0400

    set allowed hosts

--- galaxy_gaze_project/galaxygaze_django/settings.py
+++ galaxy_gaze_project/galaxygaze_django/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9#^#t)#-z_4px+sp47hqs#kb+76pj$b&feni4vzft)re46068@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -63,7 +63,7 @@ MIDDLEWARE = [
 
 # CORS_ALLOW_ALL_ORIGINS = True
 
-CORS_ALLOWED_ORIGINS = [ # <-- will need to add frontend URL once react app deployed
+CORS_ALLOWED_ORIGINS = [
     "https://galaxygaze.netlify.app",
     "http://localhost:5173",
     "http://127.0.0.1:5555",
